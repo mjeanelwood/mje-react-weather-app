f97e5dd53fe262f88aab62d74d9acf3d9ad77559
mjeanelwood <85648554+mjeanelwood@users.noreply.github.com>
2021-09-27 14:49:26 -0700

    Added github url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Coded by M J Elwood and{" "}
+        <a
+          href="https://github.com/mjeanelwood/mje-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }

01810fd76649bf76440faffe1e657331f66c4314
mjeanelwood <85648554+mjeanelwood@users.noreply.github.com>
2021-09-27 17:27:59 -0700

    Added forms and fake data to Weather.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
         <Weather />
         <footer>
           Coded by M J Elwood and{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,46 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  return <div>Hello from Weather component</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <div className="row">
+        <div className="col-4">
+          <h1>Paris</h1>
+          <ul>
+            <li>Date Time</li>
+            <li>Description</li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+          75° c | f
+        </div>
+        <div className="col-4">
+          <ul>
+            <li>Wind: 5 km/h</li>
+            <li>Humidity: 10 %</li>
+            <li>Precipitaion: 3 %</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }

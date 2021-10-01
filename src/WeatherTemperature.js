107ecde08667f0f37a9df268d78719d0f3fe3477
mjeanelwood <85648554+mjeanelwood@users.noreply.github.com>
2021-09-30 18:26:13 -0700

    Removed link underline

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -19,9 +19,9 @@ export default function WeatherTemperature(props) {
   if (unit === "celsius") {
     return (
       <div className="WeatherTemperature">
-        <span className="temperature"> {Math.round(props.celsius)}</span>{" "}
+        <span className="temperature"> {Math.round(props.celsius)}</span>
         <span className="unit">
-          °C |{" "}
+          °C |
           <a href="/" onClick={showFahrenheit}>
             °F
           </a>

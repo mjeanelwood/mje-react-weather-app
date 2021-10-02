a8d28025d7e09b62f02144ec7c45c01d2afaf1e1
mjeanelwood <85648554+mjeanelwood@users.noreply.github.com>
2021-10-01 18:20:47 -0700

    Added css to components

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,17 +7,19 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <div className="row mt-4">
-        <div className="col-4">
+        <div className="col-5">
           <h1>{props.data.city}</h1>
           <p>
             <CurrentDate date={props.data.date} />
           </p>
         </div>
         <div className="col-4">
-          <WeatherIcon code={props.data.icon} />
-          <WeatherTemperature celsius={props.data.temperature} />
+          <span className="current-temperature">
+            <WeatherIcon code={props.data.icon} />
+            <WeatherTemperature celsius={props.data.temperature} />
+          </span>
         </div>
-        <div className="col-4">
+        <div className="col-3">
           <ul>
             <li>Wind: {Math.round(props.data.wind)} km/h</li>
             <li>Humidity: {Math.round(props.data.humidity)} %</li>
